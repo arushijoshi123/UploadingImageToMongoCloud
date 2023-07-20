46e879ab9944c77419a537ce7973963c434679f2
arushijoshi123 <arushi2132joshi@gmail.com>
2023-07-21 00:20:33 +0530

    8th commit

--- frontend/components/ImageGallery.js
+++ frontend/components/ImageGallery.js
@@ -1,38 +1,39 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import styles from "./ImageGallery.module.css";
 
 const ImageGallery = ({ userId }) => {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
     // Fetch images from the server for the specific user
-    axios.get(`https://uploadimagetomongodbapi.onrender.com/${userId}`)
-      .then(res => {
+    axios
+      .get(`https://uploadimagetomongodbapi.onrender.com/image/${userId}`)
+      .then((res) => {
         // Assuming the images are inside the "allimages" field of the response object
         setImages(res.data.allimages);
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   }, [userId]); // Trigger the effect whenever userId changes
 
   return (
-    <div >
+    <div className="image-gallery">
       <h2>Uploaded Images</h2>
       {images && images.length > 0 ? (
-        <div className={styles.imageGrid}>
-          {images.map((image, index) => (
-            // Check if the 'image.image' property is defined before rendering the image
-            image.image && (
-              <img
-               
-                key={index}
-                src={`https://uploadimagetomongodbapi.onrender.com/${image.image}`}
-                alt={`Image ${index}`}
-                className={styles.imageItem}
-
-              />
-            )
-          ))}
+        <div>
+          {images.map(
+            (image, index) =>
+              // Check if the 'image.image' property is defined before rendering the image
+              image.image && (
+                <img
+                  width={100}
+                  height={100}
+                  key={index}
+                  src={`https://uploadimagetomongodbapi.onrender.com/${image.image}`}
+                  alt={`Image ${index}`}
+                  className="image-item"
+                />
+              )
+          )}
         </div>
       ) : (
         <p>No images uploaded yet.</p>
@@ -40,8 +41,5 @@ const ImageGallery = ({ userId }) => {
     </div>
   );
 };
-
-
-
 
 export default ImageGallery;
